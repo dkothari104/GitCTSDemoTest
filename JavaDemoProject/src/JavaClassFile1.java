be8383b5e2e8bdff17bb121810af12ec979e8b65
dkothari104 <dkothari@digite.com>
2017-09-28 11:07:07 +0530

    OwnerId::1000152235 entityType::AUSRST IssueId::AUSRST4354 Reviewer::admin Status::Done Comments::This is a test commit

--- JavaDemoProject/src/JavaClassFile1.java
+++ JavaDemoProject/src/JavaClassFile1.java
@@ -4,7 +4,7 @@ public class JavaClassFile1 {
 	public static void main(String[] args)
 	{
 	
-		System.out.println("Hello World Once Again..!");
+		System.out.println("Hello World Once Again.!");
 		
 	}
 
